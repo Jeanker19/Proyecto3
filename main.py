cfe9eed20b7c6791ec5c6fd1ac2a669c62252122
Jeanker19 <jcpm_22@hotmail.com>
2021-06-30 14:10:39 -0600

    Se crea la ventana de ayuda y la de ingresar nombre de usuario

--- main.py
+++ main.py
@@ -1,5 +1,64 @@
 import tkinter
 
+
+def ventanaNombre():
+    ventana = tkinter.Tk()  # Se crea la ventana
+    ventana.geometry("400x250")  # Tamanno de la ventana
+    ventana.title("Futoshiki")  # Titulo
+    titulo = tkinter.Label(ventana, text="Nombre")
+    titulo.place(x=30, y=20)
+    nombr = tkinter.Entry(ventana)
+    nombr.place(x=100, y=20)
+
+    def listo():
+        nombre[0] = nombr.get()
+        ventana.destroy()
+        ventanaMenu()
+
+    boton = tkinter.Button(ventana, text="Ok", command=listo)
+    boton.place(x=100, y=100)
+    ventana.mainloop()
+
+
+def ventanaAyuda():
+    ventana = tkinter.Tk()  # Se crea la ventana
+    ventana.geometry("1200x600")  # Tamanno de la ventana
+    ventana.title("Futoshiki")  # Titulo
+    imagen1 = tkinter.PhotoImage(file='img1.png')
+    imagen2 = tkinter.PhotoImage(file='img2.png')
+    imagen3 = tkinter.PhotoImage(file='img3.png')
+    imagen4 = tkinter.PhotoImage(file='img.png')
+    imagen5 = tkinter.PhotoImage(file='img_1.png')
+    imagen6 = tkinter.PhotoImage(file='img_2.png')
+    label1 = tkinter.Label(ventana, image=imagen1)
+    label1.place(x=0, y=0)
+    label2 = tkinter.Label(ventana, image=imagen2)
+    label2.place(x=350, y=0)
+    label3 = tkinter.Label(ventana, image=imagen3)
+    label3.place(x=750, y=0)
+
+    def volver():
+        ventana.destroy()
+        ventanaMenu()
+
+    def cambiar():
+        label1.config(image=imagen4)
+        label2.config(image=imagen5)
+        label3.config(image=imagen6)
+
+    def cambiar2():
+        label1.config(image=imagen1)
+        label2.config(image=imagen2)
+        label3.config(image=imagen3)
+
+    boton = tkinter.Button(ventana, text="Volver", command=volver)
+    boton.place(x=500, y=500)
+    boton2 = tkinter.Button(ventana, text="Siguiente Pagina", command=cambiar)
+    boton2.place(x=600, y=500)
+    boton3 = tkinter.Button(ventana, text="Pagina Anterior", command=cambiar2)
+    boton3.place(x=350, y=500)
+    ventana.mainloop()
+
 def ventanaMenu(): #menu
     ventana = tkinter.Tk()  # Se crea la ventana
     ventana.geometry("250x400")  # Tamanno de la ventana
